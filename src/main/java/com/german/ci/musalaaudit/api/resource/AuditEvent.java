a09dd23da22765f6e205a98584ebc2c9da9e2c84
german <kochgerm@gmail.com>
2022-12-02 00:26:41 +0300

    asd

--- src/main/java/com/german/ci/musalaaudit/api/resource/AuditEvent.java
+++ src/main/java/com/german/ci/musalaaudit/api/resource/AuditEvent.java
@@ -1,7 +1,4 @@
 package com.german.ci.musalaaudit.api.resource;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public record AuditEvent(String droneId, Integer batteryCapacity, State state) {
 }
